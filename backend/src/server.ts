a4f2af5dae2056bb1a5dabb1ca33ce9cd5e0b146
gabriel.gullon <gabriel.gullon@gelita-subcontractors.com>
2024-07-09 11:23:56 -0300

    add sample routes to validate prisma integration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,24 @@
-console.log("Hello world blyaaad");
+import fastify from "fastify";
+import { prisma } from "./lib/prisma";
+
+const app = fastify();
+
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: "Santiago, CL",
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  });
+
+  return "Registro cadastrado com sucesso";
+});
+
+app.get("/listar", async () => {
+  return await prisma.trip.findMany();
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running");
+});
