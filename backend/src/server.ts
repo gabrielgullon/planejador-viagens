d746db5d64d28a2903de92f7ef06ab9f0743589e
gabriel.gullon <gabriel.gullon@gelita-subcontractors.com>
2024-07-09 18:22:09 -0300

    add create trip route and confirm trip route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,35 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/trips/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/trips/confirm-trip";
 
 const app = fastify();
 
-app.get("/cadastrar", async () => {
-  await prisma.trip.create({
-    data: {
-      destination: "Santiago, CL",
-      starts_at: new Date(),
-      ends_at: new Date(),
-    },
-  });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-  return "Registro cadastrado com sucesso";
-});
+app.register(createTrip);
+app.register(confirmTrip);
 
-app.get("/listar", async () => {
-  return await prisma.trip.findMany();
-});
+// app.get("/cadastrar", async () => {
+//   await prisma.trip.create({
+//     data: {
+//       destination: "Santiago, CL",
+//       starts_at: new Date(),
+//       ends_at: new Date(),
+//     },
+//   });
+
+//   return "Registro cadastrado com sucesso";
+// });
+
+// app.get("/listar", async () => {
+//   return await prisma.trip.findMany();
+// });
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running");
