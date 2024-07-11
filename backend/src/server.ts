1016df90d0750ab14a1e5552cee15682358738be
gabriel.gullon <gabriel.gullon@gelita-subcontractors.com>
2024-07-10 21:15:49 -0300

    add routes: trip details, update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,6 +14,8 @@ import { createLink } from "./routes/links/create-link";
 import { getLinks } from "./routes/links/get-link";
 import { getParticipants } from "./routes/participants/get-participants";
 import { inviteToTrip } from "./routes/trips/invite-to-trip";
+import { updateTrip } from "./routes/trips/update-trip";
+import { getTripDetails } from "./routes/trips/get-trip-details";
 
 const app = fastify();
 
@@ -30,6 +32,8 @@ app.register(createLink);
 app.register(getLinks);
 app.register(getParticipants);
 app.register(inviteToTrip);
+app.register(updateTrip);
+app.register(getTripDetails);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running");
